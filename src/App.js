a94f24f388100f014d0d0212219a6325118719ed
gautam501 <gautamparmar03214@gmail.com>
2025-06-17 17:45:33 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import TictacToc from './Tictactoe';
+
+function App() {
+  return (
+    <>
+    <TictacToc></TictacToc>
+    </>
+  );
+}
+
+export default App;
