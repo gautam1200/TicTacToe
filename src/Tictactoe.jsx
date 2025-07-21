34508e8350f96c09227ef41c2ea6bca1380f3015
Gautam Parmar <gautamparmar03214@gmail.com>
2025-07-21 14:30:57 +0530

    last

--- src/Tictactoe.jsx
+++ src/Tictactoe.jsx
@@ -1,12 +1,12 @@
 import React, { useState } from "react";
 import player1 from "./circle.png";
 import player2 from "./cross.png";
-
+import bg from "./bg.avif";
 import { Box, Typography } from "@mui/material";
 
 
 const TictacToc = () => {
-  
+
 
   const [count, setCount] = useState(0);
   const [lock, setLock] = useState(false);
@@ -29,43 +29,10 @@ const TictacToc = () => {
     setCount(count + 1);
     checkWinner(newData);
     console.log(data);
-    
+
   };
 
 
-
-  // const checkWinner = (data) => {
-  //   if (data[0] === data[1] && data[1] === data[2] && data[0] !== "") {
-
-  //     alert(`Player ${data[0]} wins!`);
-  //   } else if (data[3] === data[4] && data[4] === data[5] && data[3] !== "") {
-
-  //     alert(`Player ${data[3]} wins!`);
-  //   } else if (data[6] === data[7] && data[7] === data[8] && data[6] !== "") {
-
-  //     alert(`Player ${data[6]} wins!`);
-  //   } else if (data[0] === data[3] && data[3] === data[6] && data[0] !== "") {
-
-  //     alert(`Player ${data[0]} wins!`);
-  //   } else if (data[1] === data[4] && data[4] === data[7] && data[1] !== "") {
-
-  //     alert(`Player ${data[1]} wins!`);
-  //   } else if (data[2] === data[5] && data[5] === data[8] && data[2] !== "") {
-
-  //     alert(`Player ${data[2]} wins!`);
-  //   } else if (data[0] === data[4] && data[4] === data[8] && data[0] !== "") {
-
-  //     alert(`Player ${data[0]} wins!`);
-  //   } else if (data[2] === data[4] && data[4] === data[6] && data[2] !== "") {
-
-  //     alert(`Player ${data[2]} wins!`);
-  //   } else if (count === 8) {
-      
-  //     alert("It's a draw!");
-  //   }
-  // };
-
-  
 
   const checkWinner = (newData) => {
     const winner = [
@@ -98,41 +65,69 @@ const TictacToc = () => {
     setCount(0);
     setData(["", "", "", "", "", "", "", "", ""]);
     const winnerElement = document.querySelector(".winner");
-        winnerElement.innerHTML = "";
+    winnerElement.innerHTML = "";
     const boxes = document.querySelectorAll(".box");
     boxes.forEach((box) => {
       box.innerHTML = "";
     });
-  }; 
+  };
 
 
   return (
     <>
-      <Box className="container">
-        <h1 className="text-center">Tic Tac Toe</h1>
+      <Box sx={{
+        width: '100%',
+        height: '100vh',
+        backgroundImage: `url(${bg})`,
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        backgroundRepeat: 'no-repeat',
+        position: 'relative'
+      }}>
 
-            <Typography variant="h6"  className="winner"></Typography>
-        <Box className="subrow">
-          <Box className="board" textAlign={"center"}>
-            <Box className="row">
-              <Box className="box" onClick={(e) => toggle(e, 0)}></Box>
-              <Box className="box" onClick={(e) => toggle(e, 1)}></Box>
-              <Box className="box" onClick={(e) => toggle(e, 2)}></Box>
+        <Box className="container"  >
+          <h1 className="text-center">Tic Tac Toe</h1>
+
+          <Typography variant="h6" className="winner"></Typography>
+          <Box className="subrow">
+            <Box className="board" textAlign={"center"}>
+              <Box className="row">
+                <Box className="box" onClick={(e) => toggle(e, 0)}></Box>
+                <Box className="box" onClick={(e) => toggle(e, 1)}></Box>
+                <Box className="box" onClick={(e) => toggle(e, 2)}></Box>
+              </Box>
+              <Box className="row">
+                <Box className="box" onClick={(e) => toggle(e, 3)}></Box>
+                <Box className="box" onClick={(e) => toggle(e, 4)}></Box>
+                <Box className="box" onClick={(e) => toggle(e, 5)}></Box>
+              </Box>
+              <Box className="row">
+                <Box className="box" onClick={(e) => toggle(e, 6)}></Box>
+                <Box className="box" onClick={(e) => toggle(e, 7)}></Box>
+                <Box className="box" onClick={(e) => toggle(e, 8)}></Box>
+              </Box>
             </Box>
-            <Box className="row">
-              <Box className="box" onClick={(e) => toggle(e, 3)}></Box>
-              <Box className="box" onClick={(e) => toggle(e, 4)}></Box>
-              <Box className="box" onClick={(e) => toggle(e, 5)}></Box>
-            </Box>
-            <Box className="row">
-              <Box className="box" onClick={(e) => toggle(e, 6)}></Box>
-              <Box className="box" onClick={(e) => toggle(e, 7)}></Box>
-              <Box className="box" onClick={(e) => toggle(e, 8)}></Box>
-            </Box>
+            <button className="reset" onClick={() => reset()}>
+              RESET
+            </button>
           </Box>
-          <button className="reset" onClick={() => reset()}>
-            RESET
-          </button>
+        </Box>
+        <Box
+          sx={{
+            width: '100%',
+            position: 'absolute',
+            bottom: '0'
+          }}>
+
+          <Typography
+            sx={{
+              textAlign: 'right',
+              fontFamily: 'math',
+              color: '#ffffff52',
+              fontSize: '20px',
+              fontWeight: 'bold',
+              padding: '5px 15px'
+            }}>Developed by Gautam Parmar</Typography>
         </Box>
       </Box>
     </>
